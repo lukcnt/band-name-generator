04fd4b798c63d0a5aef4083179434f884050296a
Lucas Cantoni <lukcnt@outlook.com>
2022-07-25 00:10:55 -0300

    Createdthe pet variable.

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ print("Welcome to the band name generator.")
 #Ask the city that the user grew up in.
 city = input("Which city did you grow up in?\n")
 
+#Ask the user for the name of a pet.
+pet = input("What is the name of a pet?\n")
