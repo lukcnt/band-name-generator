996dd2a856467f101bd55159d92998f3371fce07
Lucas Cantoni <lukcnt@outlook.com>
2022-07-25 00:16:40 -0300

    Created the output of the variables combined.

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ city = input("Which city did you grow up in?\n")
 
 #Ask the user for the name of a pet.
 pet = input("What is the name of a pet?\n")
+
+#Combine the name of their city and pet and show them their band name.
+print(f"Your band name could be {city} {pet}.")
